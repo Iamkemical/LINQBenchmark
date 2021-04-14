cb0ac92ccbe7d36efe8d8d54061c904040d7eabf
Gabriel Isaac <iamkemical1@gmail.com>
2021-04-14 01:09:41 +0100

    Added Benchmark project

---
+++ LINQBenchmarkTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace LINQBenchmarkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Student>();
+        }
+    }
+}
---
+++ LINQBenchmarkTest/Student.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQBenchmarkTest
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class Student
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public string Department { get; set; }
+
+        [Benchmark]
+        public void LINQWhere()
+        {
+            List<Student> students = new();
+            students.Add(new() { Name = "Gabriel", Age = 21, Department = "Chemical Engineering" });
+            students.Add(new() { Name = "Patrick", Age = 29, Department = "Quantity Surveying" });
+            students.Add(new() { Name = "Silas", Age = 27, Department = "Civil Engineering" });
+
+            var ages = students.Where(x => x.Age == 21);
+            foreach (var age in ages)
+            {
+                Console.WriteLine(age.Name);
+            } 
+        }
+
+        [Benchmark]
+        public void LINQFirst()
+        {
+            List<Student> students = new();
+            students.Add(new() { Name = "Gabriel", Age = 21, Department = "Chemical Engineering" });
+            students.Add(new() { Name = "Patrick", Age = 29, Department = "Quantity Surveying" });
+            students.Add(new() { Name = "Silas", Age = 27, Department = "Civil Engineering" });
+
+            var ages = students.FirstOrDefault(x => x.Age == 21);
+            Console.WriteLine(ages.Name);
+        }
+    }
+}
